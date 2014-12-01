1b44d942ad011600873bbce5cfacfc2aa8ec44bf
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-01 14:21:42 -0600

    Basic Player Turn Set Up

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+// ;(function(){
+//   "use strict";
+var myModule = {
+  turn: true,
+  gameTurn: function(){
+    if(this.turn = true){
+      this.turn = false;
+    } else {
+      this.turn = true;
+    }
+  },
+}
+
+// }());

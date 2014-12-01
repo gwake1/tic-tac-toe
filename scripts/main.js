34dfae083791b34022def7252b5d7f04a3f7f375
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-01 15:30:25 -0600

    Basic Functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,39 @@
 // ;(function(){
 //   "use strict";
-var myModule = {
-  turn: true,
-  gameTurn: function(){
-    if(this.turn = true){
-      this.turn = false;
-    } else {
-      this.turn = true;
+var myModule = (function(){
+  var turn = true;
+  var player = {
+    one: {
+      name: "",
+      turn: true
+    },
+    two: {
+      name: "",
+      turn: false
+    },
+    gameTurn: function(turn){
+      if(turn === true){
+        turn = false;
+      } else {
+        turn = true;
+      }
+      return turn;
+    },
+    selected: $("td").click(function(){
+      $(this).addClass("selected");
+    }),
+    append: $("td").click(function(){
+      if(turn === true && $(this).hasClass("selected")){
+        $(this).addClass("tic");
+        $(this).removeClass("selected");
+        myModule.gameTurn(turn);
+      } else {
+        $(this).addClass("tac");
+        $(this).removeClass("selected");
+        myModule.gameTurn(turn);
+      }
+    }),
+    gameOver: function(){
     }
-  },
-}
-
-// }());
+  }
+})();

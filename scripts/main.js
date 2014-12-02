df88da7528fcab46eadfaae338a71b0121ed7f3a
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-01 21:00:48 -0600

    WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 // ;(function(){
 //   "use strict";
-var myModule = (function(){
-  var turn = true;
-  var player = {
+var playerTurn ={
+  turn: true,
+  sign: "tic",
+  player: {
     one: {
       name: "",
       turn: true
@@ -10,30 +11,48 @@ var myModule = (function(){
     two: {
       name: "",
       turn: false
-    },
-    gameTurn: function(turn){
-      if(turn === true){
-        turn = false;
-      } else {
-        turn = true;
-      }
-      return turn;
-    },
-    selected: $("td").click(function(){
-      $(this).addClass("selected");
-    }),
-    append: $("td").click(function(){
-      if(turn === true && $(this).hasClass("selected")){
-        $(this).addClass("tic");
-        $(this).removeClass("selected");
-        myModule.gameTurn(turn);
-      } else {
-        $(this).addClass("tac");
-        $(this).removeClass("selected");
-        myModule.gameTurn(turn);
-      }
-    }),
-    gameOver: function(){
     }
   }
+}
+
+var module = (function () {
+  var gameCounter, privateGameCounter, append;
+  gameCounter = 0;
+  append = function( foo ){
+    var foo = gameCounter;
+    if(foo%2===0){
+      $("td").on("click", function( foo ){
+        $(this).addClass("tic");
+        return console.log("tic");
+      })
+    } else if (foo%2===1){
+      $(this).addClass("tac");
+      return console.log("tac");
+    }
+  }
+  return {
+    myPublicVar: "foo",
+    myPublicFunction: $("td").click(function(){
+      append( "bar" );
+      gameCounter++;
+    })
+  }
 })();
+
+// (function append(){
+//   $("td").click(function(){
+//     if(playerTurn.turn === true && $(this).hasClass("empty")){
+//       $(this).addClass("tic");
+//       $(this).removeClass("selected");
+//       gameTurn();
+//       $(this).removeClass("empty");
+//       winner();
+//     } else if (playerTurn.turn === false && $(this).hasClass("empty")){
+//       $(this).addClass("tac");
+//       $(this).removeClass("selected");
+//       gameTurn();
+//       $(this).removeClass("empty");
+//       winner();
+//     }
+//   })
+// }());
